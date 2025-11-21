437354cd40326f5a16f6fd09a076ce6eb5f18fa5
Eleven <yangjiayi0920@outlook.com>
2025-11-21 02:59:34 -0500

    Format changed

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,27 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Class representing a performance of a play.
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private String playID;
+    private int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
+        this.setAudience(audience);
+    }
+
+    public String getPlayID() {
+        return playID;
+    }
+
+    public int getAudience() {
+        return audience;
+    }
+
+    public void setAudience(int audience) {
         this.audience = audience;
     }
 }
